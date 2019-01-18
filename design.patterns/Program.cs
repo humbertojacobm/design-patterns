bdbbb3a25eea2cff3acbd76dafdf3875ec2b2333
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-01-18 18:48:02 -0500

    modulo 01 solid video 03 single responsability

--- design.patterns/Program.cs
+++ design.patterns/Program.cs
@@ -1,15 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace design.patterns
 {
-    class Program
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;//memennto
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, j.ToString());
+        }
+    }
+    public class Program
     {
         static void Main(string[] args)
         {
+            var j = new Journal();
+            j.AddEntry("I cried today");
+            j.AddEntry("I ate a bug");
+            Console.WriteLine(j);
+            Console.ReadKey();
+
+            var p = new Persistence();
+            var filename = @"c:\temp\journal.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename);
+            Console.ReadKey();
         }
     }
 }
