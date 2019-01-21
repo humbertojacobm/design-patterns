b867f065f14fff07984f9b460f9c7c5d69284200
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-01-21 17:29:11 -0500

    modulo 01 solid video 04 open closed principle part 1 : bad practice

--- design.patterns/Program.cs
+++ design.patterns/Program.cs
@@ -5,59 +5,86 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace design.patterns
 {
-    public class Journal
+    public enum Color
     {
-        private readonly List<string> entries = new List<string>();
-        private static int count = 0;
+        Red,
+        Green,
+        Blue
+    }
 
-        public int AddEntry(string text)
-        {
-            entries.Add($"{++count}: {text}");
-            return count;//memennto
-        }
+    public enum Size
+    {
+        Small, Medium, Large, Yuge
+    }
 
-        public void RemoveEntry(int index)
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+        public Product(string name, Color color, Size size)
         {
-            entries.RemoveAt(index);
-        }
-
-        public override string ToString()
-        {
-            return string.Join(Environment.NewLine, entries);
-        }
-
-        public void Save(string filename)
-        {
-            File.WriteAllText(filename, ToString());
+            if (name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+            Name = name;
+            Color = color;
+            Size = size;
         }
     }
 
-    public class Persistence
+    public class ProductFilter
     {
-        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products,
+            Size size)
         {
-            if (overwrite || !File.Exists(filename))
-                File.WriteAllText(filename, j.ToString());
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
         }
+
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products,
+            Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+
+        public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products,
+            Size size,Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color && p.Size == size)
+                    yield return p;
+        }
+
     }
+
     public class Program
     {
         static void Main(string[] args)
         {
-            var j = new Journal();
-            j.AddEntry("I cried today");
-            j.AddEntry("I ate a bug");
-            Console.WriteLine(j);
-            Console.ReadKey();
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
 
-            var p = new Persistence();
-            var filename = @"c:\temp\journal.txt";
-            p.SaveToFile(j, filename, true);
-            Process.Start(filename);
-            Console.ReadKey();
+            Product[] products = {apple, tree, house};
+            var pf = new ProductFilter();
+            WriteLine("Green products (old):");
+
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                WriteLine($" - {p.Name} is green");
+            }
+
+            ReadKey();
+
         }
     }
 }
